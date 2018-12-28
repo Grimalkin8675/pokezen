20b123f5b8ae6ba4b94ba52ae5795ca8f6f34fd9
Axel Nussbaumer <nussbaume@eisti.eu>
2018-12-28 21:39:27 +0100

    add search route

--- back/test/ApplicationSpec.scala
+++ back/test/ApplicationSpec.scala
@@ -2,16 +2,32 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+
+import tests.MockSearchService
+import pokezen.controllers.SearcheableService
 
 
 class ApplicationSpec extends PlaySpec with GuiceOneAppPerTest {
+  override def fakeApplication() = new GuiceApplicationBuilder()
+    .overrides(bind[SearcheableService].to[MockSearchService])
+    .build()
+
   "Routes" should {
     "send 404 on a bad request (/)" in {
-      route(app, FakeRequest(GET, "/")).map(status(_)) mustBe Some(NOT_FOUND)
+      route(app, FakeRequest(GET, "/"))
+        .map(status(_)) mustBe Some(NOT_FOUND)
     }
 
     "send 404 on a bad request (/boum)" in {
-      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+      route(app, FakeRequest(GET, "/boum"))
+        .map(status(_)) mustBe Some(NOT_FOUND)
+    }
+
+    "send 200 on a valid request (/search/whatever)" in {
+      route(app, FakeRequest(GET, "/search/whatever"))
+        .map(status(_)) mustBe Some(OK)
     }
   }
 }
