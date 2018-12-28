89b3c17c39ef891114bf62054a55de03dfb91672
Axel Nussbaumer <nussbaume@eisti.eu>
2018-12-28 19:55:34 +0100

    removed MainController

---
+++ back/test/ApplicationSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
+
+
+class ApplicationSpec extends PlaySpec with GuiceOneAppPerTest {
+  "Routes" should {
+    "send 404 on a bad request (/)" in {
+      route(app, FakeRequest(GET, "/")).map(status(_)) mustBe Some(NOT_FOUND)
+    }
+  }
+
+  "Routes" should {
+    "send 404 on a bad request (/boum)" in {
+      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+    }
+  }
+}
