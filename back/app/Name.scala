7bc5e2f973a64633965663876c9f32ee22302244
Axel Nussbaumer <nussbaume@eisti.eu>
2018-12-28 19:00:56 +0100

    add Writes[Name]

--- back/app/Name.scala
+++ back/app/Name.scala
@@ -1,4 +1,12 @@
 package pokezen
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 
 case class Name(name: String)
+
+object Name {
+  implicit val nameWrites: Writes[Name] =
+    Writes((name: Name) => JsString(name.name))
+}
--- back/test/NameSpec.scala
+++ back/test/NameSpec.scala
@@ -1,8 +1,17 @@
 import org.scalacheck._
 import org.scalacheck.Prop._
 
+import play.api.libs.json._
+
 import pokezen.Name
 
 object NameSpec extends Properties("Name") {
   property("name") = Name("foo").name == "foo"
+
+  property(" stringifying") =
+    Json.toJson(Name("foo")).toString == """"foo""""
+
+  property(" stringifying list") =
+    Json.toJson(List(Name("foo"), Name("bar"))).toString ==
+      """["foo","bar"]"""
 }
