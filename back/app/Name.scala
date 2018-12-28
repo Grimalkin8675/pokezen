cdb9d68d85795d9dbe69d623080c58acc11efbc3
Axel Nussbaumer <nussbaume@eisti.eu>
2018-12-28 23:07:52 +0100

    add Name.contains(string)

--- back/app/Name.scala
+++ back/app/Name.scala
@@ -4,7 +4,9 @@ import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
 
-case class Name(name: String)
+case class Name(name: String) {
+  def contains(string: String): Boolean = this.name.contains(string)
+}
 
 object Name {
   implicit val nameWrites: Writes[Name] =
