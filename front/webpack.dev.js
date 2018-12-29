e7074958fa8102357d5d31a9cf8385220d758b0d
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2018-12-29 22:43:38 +0100

    changed front container exposed port from 8080 to 51265

--- front/webpack.dev.js
+++ front/webpack.dev.js
@@ -8,7 +8,7 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
-        port: 8080,
+        port: 51265,
         contentBase: common.output.path,
         inline: true, // iframe or inline script
         host: '0.0.0.0',
