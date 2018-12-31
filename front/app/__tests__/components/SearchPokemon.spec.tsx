ae463f1f298590e105fa744a1c8b69f7b6a959de
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2018-12-31 12:55:58 +0100

    refacto SearchPokemon

--- front/app/__tests__/components/SearchPokemon.spec.tsx
+++ front/app/__tests__/components/SearchPokemon.spec.tsx
@@ -7,10 +7,8 @@ import SearchPokemon from '../../components/SearchPokemon';
 describe(SearchPokemon, () => {
     describe('render', () => {
         it('should render', () => {
-            const searchPokemon = shallow(<SearchPokemon />);
-            expect(searchPokemon.contains(
-                <input type='text' placeholder='Search for pokemons!' />)
-            ).toBe(true);
+            const searchPokemon = shallow(<SearchPokemon/>);
+            expect(searchPokemon.find('input').prop('type')).toBe('text');
         });
     });
 });
