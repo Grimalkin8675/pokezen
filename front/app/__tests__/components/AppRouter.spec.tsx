e254f69ee637630c94af52bf2abe9419f79d7adf
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2018-12-31 13:21:20 +0100

    add NotFound404

--- front/app/__tests__/components/AppRouter.spec.tsx
+++ front/app/__tests__/components/AppRouter.spec.tsx
@@ -4,20 +4,29 @@ import { MemoryRouter } from 'react-router-dom';
 
 import AppRouter from '../../components/AppRouter';
 import SearchPokemon from '../../components/SearchPokemon';
+import NotFound404 from '../../components/NotFound404';
 
 
 describe(AppRouter, () => {
     describe('route /', () => {
+        const defaultRoute = shallow(
+            <MemoryRouter initialEntries={['/']}>
+                <AppRouter/>
+            </MemoryRouter>
+        );
+
         it('should contain SearchPokemon component', () => {
-            const router = shallow(
-                <MemoryRouter initialEntries={['/']}>
-                    <AppRouter/>
-                </MemoryRouter>
-            );
             const searchPokemon = shallow(<SearchPokemon/>);
-            const routerIncludesSearchPokemon =
-                router.html().includes(searchPokemon.html());
-            expect(routerIncludesSearchPokemon).toBe(true);
+            const defaultRouteIncludesSearchPokemon =
+                defaultRoute.html().includes(searchPokemon.html());
+            expect(defaultRouteIncludesSearchPokemon).toBe(true);
+        });
+
+        it('shouldn\'t contain NotFound404 component', () => {
+            const notFound404 = shallow(<NotFound404/>);
+            const defaultRouteIncludesNotFound404 =
+                defaultRoute.html().includes(notFound404.html());
+            expect(defaultRouteIncludesNotFound404).toBe(false);
         });
     });
 });
---
+++ front/app/components/NotFound404.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+
+export default class NotFound404 extends React.Component {
+    render() {
+        return (
+            <div className='NotFound404'>
+                Route not found.
+            </div>
+        );
+    }
+}
