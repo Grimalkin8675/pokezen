9ff2ca30b9ddb7e1c3545abf427e1734495d3877
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-01 19:31:48 +0100

    refacto AppRouter

--- front/app/components/AppRouter.tsx
+++ front/app/components/AppRouter.tsx
@@ -3,14 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import SearchPokemon from './SearchPokemon';
 import NotMatch from './NotMatch';
+import pokemonsGetter from '../__mocks__/pokemonsGetter';
 
 
 export default class AppRouter extends React.Component {
     render() {
+        const searchPokemon = () => <SearchPokemon getter={pokemonsGetter}/>;
+        const notMatch = () => <NotMatch/>;
+
         return (
             <Switch>
-                <Route path='/' exact={true} component={SearchPokemon}/>
-                <Route component={NotMatch}/>
+                <Route path='/' exact={true} render={searchPokemon}/>
+                <Route render={notMatch}/>
             </Switch>
         );
     }
