c31d67d5891cda128d67d9bc962ad019cf4ef1bd
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-01 13:45:15 +0100

    refacto NameOrdering

--- back/app/Name.scala
+++ back/app/Name.scala
@@ -6,9 +6,11 @@ import play.api.libs.functional.syntax._
 
 case class Name(name: String)
 
-object Name extends Ordering[Name] {
-  implicit def compare(a: Name, b: Name): Int = a.name compare b.name
-
+object Name {
   implicit val nameWrites: Writes[Name] =
     Writes((name: Name) => JsString(name.name))
+}
+
+object NameOrdering extends Ordering[Name] {
+  def compare(a: Name, b: Name): Int = a.name compare b.name
 }
--- back/test/NameSpec.scala
+++ back/test/NameSpec.scala
@@ -3,20 +3,11 @@ import org.scalacheck.Prop._
 
 import play.api.libs.json._
 
-import pokezen.Name
+import pokezen.{Name, NameOrdering}
 
 
 object NameSpec extends Properties("Name") {
   property("name") = Name("foo").name == "foo"
-
-  property("compare(a, b)") =
-    Name.compare(Name("foo"), Name("bar")) > 0
-
-  property("compare(a, b)") =
-    Name.compare(Name("foo"), Name("foo")) == 0
-
-  property("compare(a, b)") =
-    Name.compare(Name("bar"), Name("foo")) < 0
 
   property(" stringifying") =
     Json.toJson(Name("foo")).toString == """"foo""""
@@ -24,4 +15,15 @@ object NameSpec extends Properties("Name") {
   property(" stringifying list") =
     Json.toJson(List(Name("foo"), Name("bar"))).toString ==
       """["foo","bar"]"""
+}
+
+object NameOrderingSpec extends Properties("NameOrdering") {
+  property("compare(a, b)") =
+    NameOrdering.compare(Name("foo"), Name("bar")) > 0
+
+  property("compare(a, b)") =
+    NameOrdering.compare(Name("foo"), Name("foo")) == 0
+
+  property("compare(a, b)") =
+    NameOrdering.compare(Name("bar"), Name("foo")) < 0
 }
