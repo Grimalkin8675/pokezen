353e54470dc052eb817b2a3787a3ccabf0825649
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-01 19:46:33 +0100

    add BrowserRouter arround AppRouter

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -2,6 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import AppRouter from './components/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<AppRouter/>, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <AppRouter/>
+    </BrowserRouter>), document.getElementById('root'));
