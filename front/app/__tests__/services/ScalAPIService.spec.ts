637c0f1bb92a71bdd028fb5105babfb2bf354428
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-09 14:08:43 +0100

    changed ScalAPIService constructor signature

--- front/app/__tests__/services/ScalAPIService.spec.ts
+++ front/app/__tests__/services/ScalAPIService.spec.ts
@@ -1,10 +1,17 @@
-import ScalAPIService from '../../services/ScalAPIService';
+import ScalAPIService, { IWSClient } from '../../services/ScalAPIService';
+import Name from '../../Name';
+import Names from '../../Names';
 
 
 describe(ScalAPIService, () => {
+    const getter: IWSClient = {
+        get: () => new Promise(resolve =>
+            resolve(new Names(new Name('foo')))),
+    };
+
     describe('constructor', () => {
         it('should create a ScalAPIService', () => {
-            const service = new ScalAPIService();
+            const service = new ScalAPIService(getter);
             expect(service).toBeInstanceOf(ScalAPIService);
         });
     });
--- front/app/services/ScalAPIService.ts
+++ front/app/services/ScalAPIService.ts
@@ -1,5 +1,9 @@
+export interface IWSClient {
+    get: (url: string) => Promise<any>;
+}
+
 export default class ScalAPIService {
-    constructor(/*wsClient: IWSClient */) {
-        // this.wsClient = wsClient;
+    constructor(wsClient: IWSClient) {
+        //
     }
 }
