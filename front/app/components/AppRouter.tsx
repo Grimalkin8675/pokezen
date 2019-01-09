73436f4117649839bd693f1fdc10675ebce34c11
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-09 13:54:20 +0100

    add injection to AppRouter

--- front/app/components/AppRouter.tsx
+++ front/app/components/AppRouter.tsx
@@ -1,18 +1,19 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import SearchPokemon from './SearchPokemon';
+import SearchPokemon, { IPokemonsGetter } from './SearchPokemon';
 import NotMatch from './NotMatch';
-import pokemonsGetter from '../__mocks__/pokemonsGetter';
 
 
 interface IProps {
     getRouter: (child: JSX.Element) => JSX.Element;
+    pokemonsGetter: IPokemonsGetter;
 }
 
 export default class AppRouter extends React.Component<IProps> {
     render() {
-        const searchPokemon = () => <SearchPokemon getter={pokemonsGetter}/>;
+        const searchPokemon = () =>
+            <SearchPokemon getter={this.props.pokemonsGetter}/>;
         const notMatch = () => <NotMatch/>;
 
         return this.props.getRouter(
