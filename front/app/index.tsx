e6b6b4ff36a7f711d8706565edeb85c8329bc37c
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-09 15:02:08 +0100

    removing mock from main entry

--- front/app/index.tsx
+++ front/app/index.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import AppRouter from './components/AppRouter';
-import pokemonsGetter from './__mocks__/pokemonsGetter';
+import ScalAPIService from './services/ScalAPIService';
 
 
 const router = (child: JSX.Element) => <BrowserRouter>{child}</BrowserRouter>;
 ReactDOM.render(
     <AppRouter getRouter={router}
-               pokemonsGetter={pokemonsGetter} />,
+               pokemonsGetter={new ScalAPIService(axios)} />,
     document.getElementById('root')
 );
