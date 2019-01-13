f7fdacb1461953eeb8f989f1f423119eea5eb29f
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-13 16:40:45 +0100

    add json Reads

--- back/app/services/pokeapi/PokeAPIPokemon.scala
+++ back/app/services/pokeapi/PokeAPIPokemon.scala
@@ -3,7 +3,7 @@ package pokezen.services.pokeapi
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-import pokezen.{Pokemon, PokemonName, ImageURL, Types, Stats}
+import pokezen.{Pokemon, PokemonName, ImageURL, Types, Type, Stats, Stat}
 
 
 class PokeAPIPokemon(
@@ -19,4 +19,26 @@ object PokeAPIPokemon {
       types: Types,
       baseStats: Stats): PokeAPIPokemon =
     new PokeAPIPokemon(name, image, types, baseStats)
+
+  implicit val typeReads: Reads[Type] =
+    (__ \ "type" \ "name").read[String].map(Type.apply)
+
+  implicit val statReads: Reads[Stat] = (
+    (__ \ "stat" \ "name").read[String] and
+    (__ \ "base_stat").read[Double]
+  )(Stat.apply _)
+
+  implicit val pokeAPIPokemonReads: Reads[PokeAPIPokemon] = (
+    (__ \ "name").read[String] and
+    (__ \ "sprites" \ "front_default").read[String] and
+    (__ \ "types").read[Seq[Type]] and
+    (__ \ "stats").read[Seq[Stat]]
+  )(
+    (name: String, image: String, types: Seq[Type], stats: Seq[Stat]) =>
+      PokeAPIPokemon(
+        PokemonName(name),
+        ImageURL(image),
+        Types(types: _*),
+        Stats(stats: _*))
+  )
 }
--- back/test/services/pokeapi/PokeAPIPokemonSpec.scala
+++ back/test/services/pokeapi/PokeAPIPokemonSpec.scala
@@ -3,7 +3,7 @@ import org.scalacheck.Prop._
 
 import play.api.libs.json._
 
-import pokezen.{Pokemon, PokemonName, ImageURL, Types, Stats}
+import pokezen._
 import pokezen.services.pokeapi.PokeAPIPokemon
 
 
@@ -11,4 +11,57 @@ object PokeAPIPokemonSpec extends Properties("PokeAPIPokemon") {
   property(" Is of type Pokemon") =
     PokeAPIPokemon(PokemonName(""), ImageURL(""), Types(), Stats())
       .isInstanceOf[Pokemon]
+
+  property(" Parsing from json") = {
+    val json = """
+      {
+        "abilities": [],
+        "base_experience": -1,
+        "forms": [],
+        "game_indices": [],
+        "name": "foo",
+        "sprites": {
+          "front_default": "foo image url",
+          "back_default": "not used field"
+        },
+        "types": [
+          {
+            "slot": 2,
+            "type": {
+              "name": "poison",
+              "url": "some url"
+            }
+          },
+          {
+            "type": {
+              "name": "grass"
+            }
+          }
+        ],
+        "stats": [
+          {
+            "base_stat": 45,
+            "effort": 0,
+            "stat": {
+              "name": "speed",
+              "url": "other url"
+            }
+          },
+          {
+            "base_stat": 65,
+            "stat": {
+              "name": "defense"
+            }
+          }
+        ]
+      }
+    """
+    Json.parse(json).validate[PokeAPIPokemon].asOpt
+      .exists(_ ==
+        Pokemon(
+          PokemonName("foo"),
+          ImageURL("foo image url"),
+          Types(Type("poison"), Type("grass")),
+          Stats(Stat("speed", 45), Stat("defense", 65))))
+  }
 }
