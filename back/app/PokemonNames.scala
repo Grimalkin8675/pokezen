8470e5a8e06c7d991e5e60c76ec759e2923d1831
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-13 12:28:24 +0100

    PokemonNames to json

--- back/app/PokemonNames.scala
+++ back/app/PokemonNames.scala
@@ -1,4 +1,12 @@
 package pokezen
 
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
+
 
 case class PokemonNames(names: PokemonName*)
+
+object PokemonNames {
+  implicit val namesWrites: Writes[PokemonNames] =
+    Writes((names: PokemonNames) => Json.toJson(names.names))
+}
--- back/test/PokemonNamesSpec.scala
+++ back/test/PokemonNamesSpec.scala
@@ -10,4 +10,9 @@ object PokemonNamesSpec extends Properties("PokemonNames") {
   property("name") =
     PokemonNames(PokemonName("foo"), PokemonName("bar"))
     .names == List(PokemonName("foo"), PokemonName("bar"))
+
+  property(" stringifying list") = {
+    val res = PokemonNames(PokemonName("foo"), PokemonName("bar"))
+    Json.toJson(res).toString == """["foo","bar"]"""
+  }
 }
