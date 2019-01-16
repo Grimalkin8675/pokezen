6840549ca899a02e6b4cb573b768bb37e97f3ef1
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-16 14:45:14 +0100

    add MockDetailsService implementation

--- back/test/services/MockDetailsService.scala
+++ back/test/services/MockDetailsService.scala
@@ -2,11 +2,48 @@ package tests
 
 import scala.concurrent._
 
-import pokezen.{Pokemon, PokemonNames, PokemonName, Type}
+import pokezen._
 import pokezen.controllers.DetaileableService
 
 
 case class MockDetailsService() extends DetaileableService {
-  def pokemonByName(name: PokemonName): Future[Pokemon] = ???
-  def pokemonsOfType(pokemonType: Type): Future[PokemonNames] = ???
+  implicit val ec = ExecutionContext.global
+
+  val foo = Pokemon(PokemonName("whatever"), ImageURL(""), Types(), Stats())
+
+  val bar = Pokemon(
+    PokemonName("bar"),
+    ImageURL("bar_image"),
+    Types(Type("fire"), Type("air")),
+    Stats(Stat("speed", 70), Stat("defense", 50)))
+
+  val otherFirePokemon = Pokemon(
+    PokemonName("otherFirePokemon"),
+    ImageURL(""),
+    Types(Type("fire")),
+    Stats(Stat("speed", 60), Stat("defense", 80)))
+
+  val otherAirPokemon = Pokemon(
+    PokemonName("otherAirPokemon"),
+    ImageURL(""),
+    Types(Type("air")),
+    Stats(Stat("speed", 90), Stat("defense", 30)))
+
+  def pokemonByName(name: PokemonName): Future[Pokemon] = Future {
+    name match {
+      case PokemonName("foo") => foo
+      case PokemonName("bar") => bar
+      case PokemonName("otherFirePokemon") => otherFirePokemon
+      case PokemonName("otherAirPokemon") => otherAirPokemon
+      case _ => throw new Exception(s"pokemon not found: ${name}")
+    }
+  }
+
+  def pokemonsOfType(pokemonType: Type): Future[PokemonNames] = Future {
+    pokemonType match {
+      case Type("fire") => PokemonNames(bar.name, otherFirePokemon.name)
+      case Type("air") => PokemonNames(bar.name, otherAirPokemon.name)
+      case _ => throw new Exception(s"pokemon type not found: ${pokemonType}")
+    }
+  }
 }
