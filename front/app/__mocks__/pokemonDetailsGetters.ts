1cecf3d8cb2bed35a065a753e543c53d3b4628d1
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-21 18:35:24 +0100

    fix: Pokemon component test

---
+++ front/app/__mocks__/pokemonDetailsGetters.ts
@@ -0,0 +1,27 @@
+import { IPokemonDetailsGetter } from '../components/Pokemon';
+import Name from '../Name';
+import ComparedPokemon from '../ComparedPokemon';
+import ImageURL from '../ImageURL';
+import Types from '../Types';
+import Type from '../Type';
+import Stats from '../Stats';
+import Stat from '../Stat';
+import ComparedStat from '../ComparedStat';
+
+
+export const resolveFoo: IPokemonDetailsGetter = {
+    pokemonDetails: (name: Name) => new Promise(resolve => resolve(
+        new ComparedPokemon(
+            name,
+            new ImageURL('some url'),
+            new Types(new Type('fire')),
+            new Stats(new Stat('def', 50)),
+            new ComparedStat(
+                'def',
+                {
+                    fire: 3
+                }
+            )
+        )
+    ))
+};
--- front/app/__tests__/components/Pokemon.spec.tsx
+++ front/app/__tests__/components/Pokemon.spec.tsx
@@ -3,13 +3,15 @@ import { shallow } from 'enzyme';
 
 import Pokemon from '../../components/Pokemon';
 import Name from '../../Name';
+import { resolveFoo } from '../../__mocks__/pokemonDetailsGetters';
 
 
 describe(Pokemon, () => {
     describe('render', () => {
         it('should contain pokemon\'s name', () => {
             const searchPokemon = shallow(
-                <Pokemon name={new Name('foo')} />
+                <Pokemon name={new Name('foo')}
+                         getter={resolveFoo} />
             );
             expect(searchPokemon.html().includes('Foo')).toBe(true);
         });
