e5b718e89c207d36d8b8eb3dce13266d938ab3b0
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-24 17:33:29 +0100

    adapting jest tests to css modules

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     setupTestFrameworkScriptFile: './test-setup.js',
+    moduleNameMapper: {
+        '^.+\\.(css|less|scss)$': path.resolve(__dirname, '__mocks__/stylesMock.js'),
+    },
 };
