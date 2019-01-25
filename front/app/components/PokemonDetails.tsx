267522ad1b16a2bcdfc543b7a16dda6154abf20b
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-25 16:41:54 +0100

    add PokemonDetails styling

--- front/app/components/PokemonDetails.tsx
+++ front/app/components/PokemonDetails.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
 import ComparedPokemon from '../ComparedPokemon';
+import Stat from '../Stat';
 import Name from '../Name';
 import Type from '../Type';
 
 import * as styles from './styles/PokemonDetails.css';
+import { hoverComparedStat } from '../utils';
 
 
 export interface IPokemonDetailsGetter {
@@ -38,7 +40,7 @@ export default class PokemonDetails extends React.Component<IProps, IState> {
 
     render() {
         return (
-            <div className={styles.pokemonDetails}>
+            <div>
                 {this.state.pokemon.body(this.props.name)}
             </div>
         );
@@ -64,29 +66,81 @@ class GotPokemon implements IBodyGetter {
     }
 
     body(): JSX.Element {
+        const nameImage = (
+            <div className={styles.nameImage}>
+                <h1>{this.pokemon.name.upper()}</h1>
+                <div className={styles.image}>
+                    {this.pokemon.image !== null
+                        ? <img src={this.pokemon.image.toString()}
+                               alt={this.pokemon.name.upper()} />
+                        : <div className={styles.emptyImage}>?</div>}
+                </div>
+            </div>);
+
+        const thead = (
+            <thead>
+                <tr>
+                    <th colSpan={2}
+                        className={styles.datasTitle}>
+                        Type{this.pokemon.types.length > 1 ? 's' : ''}
+                    </th>
+                    {this.pokemon.types.length !== 0
+                        ? this.pokemon.types.map((type, i) =>
+                              <th key={i}>{type.upper()}</th>)
+                        : 'unknown'}
+                </tr>
+                <tr>
+                    <th className={styles.datasTitle}>Stats</th>
+                    <th className={styles.baseStatsTh}>base</th>
+                </tr>
+            </thead>);
+
+        const stats =
+            this.pokemon.baseStats
+                .sort((a, b) => a.name > b.name ? 1 : -1)
+                .map(this.stat);
+
         return (
             <React.Fragment>
-                <header>
-                    <h1>{this.pokemon.name.upper().toString()}</h1>
-                    <div className='image'>
-                        {this.pokemon.image !== null
-                            ? <img src={this.pokemon.image.toString()}
-                                alt={this.pokemon.name.toString()} />
-                            : null}
-                    </div>
-                </header>
-                {/* <div dangerouslySetInnerHTML={{ __html: JSON.stringify(this.pokemon, undefined, 2).replace(/\n/g, '<br>').replace(/ /g, '&nbsp;') }} /> */}
-                <table>
-                    <thead>
-                        <tr>
-                            <th/>
-                            {this.pokemon.types.map((type: Type, i: number) =>
-                                <th key={i}>{type.toString()}</th>)}
-                        </tr>
-                    </thead>
+                {nameImage}
+                <table className={styles.stats}>
+                    {thead}
+                    <tbody>
+                        {stats}
+                    </tbody>
                 </table>
             </React.Fragment>
         );
+    }
+
+    private stat = (stat: Stat, i: number): JSX.Element => {
+        const comparedStat = (type: Type, j: number): JSX.Element => {
+            const value = this.pokemon.comparedStatToType(stat, type);
+            if (value === undefined) return <div key={j} />;
+            const rounded = Math.round(value);
+            const color: string | undefined = (() => {
+                if (rounded < 0) return styles.red;
+                if (rounded > 0) return styles.green;
+                return undefined;
+            })();
+            return (
+                <td key={j}
+                    className={styles.comparedStat+' '+color}
+                    title={hoverComparedStat(
+                        this.pokemon.name,
+                        new Stat(stat.name, rounded),
+                        type
+                    )}>
+                    {rounded}
+                </td>);
+        };
+
+        return (
+            <tr key={i}>
+                <td>{stat.upper()}</td>
+                <td className={styles.baseStat}>{stat.value}</td>
+                {this.pokemon.types.map(comparedStat)}
+            </tr>);
     }
 }
 
